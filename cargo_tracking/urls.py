d91929b9ac64b8355fbe8d735e9f7e6c970d59a1
skyryzens <m.kerim600@icloud.com>
2023-04-15 13:03:15 +0300

    Front-End Sync

--- cargo_tracking/urls.py
+++ cargo_tracking/urls.py
@@ -1,5 +1,19 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path("", views.anasayfa, name="anasayfa"),
+    path("adminpaneli", views.adminpaneli, name="adminpaneli"),
+    path("girisyap", views.girisyap, name="girisyap"),
+    path("gonderiyonet", views.gonderiyonet, name="gonderiyonet"),
+    path("hakkimizda", views.hakkimizda, name="hakkimizda"),
+    path("hesapayarlari", views.hesapayarlari, name="hesapayarlari"),
+    path("kayitol", views.kayitol, name="kayitol"),
+    path("kullanicipaneli", views.kullanicipaneli, name="kullanicipaneli"),
+    path("kuryepaneli", views.kuryepaneli, name="kuryepaneli"),
+    path("kuryetakip", views.kuryetakip, name="kuryetakip"),
+    path("navbar", views.navbar, name="navbar"),
+    path("tamamlanmis", views.tamamlanmis, name="tamamlanmis"),
+    path("yenigonderim", views.yenigonderim, name="yenigonderim"),
     
 ]
--- cargo_tracking/views.py
+++ cargo_tracking/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def anasayfa(request):
+    return render(request, "anasayfa.html") 
+
+def adminpaneli(request):
+    return render(request, "adminpaneli.html")
+
+def girisyap(request):
+    return render(request, "girisyap.html")
+
+def gonderiyonet(request):
+    return render(request, "gonderiyonet.html")
+
+def hakkimizda(request):
+    return render(request, "hakkimizda.html")
+
+def hesapayarlari(request):
+    return render(request, "hesapayarlari.html")
+
+def kayitol(request):
+    return render(request, "kayitol.html")
+
+def kullanicipaneli(request):
+    return render(request, "kullanicipaneli.html")
+
+def kuryepaneli(request):
+    return render(request, "kuryepaneli.html")
+
+def kuryetakip(request):
+    return render(request, "kuryetakip.html")
+
+def tamamlanmis(request):
+    return render(request, "tamamlanmis.html")
+
+def yenigonderim(request):
+    return render(request, "yenigonderim.html")
+
+def navbar(request):
+    return render(request, "navbar.html")
